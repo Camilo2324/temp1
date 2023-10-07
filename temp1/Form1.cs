11237db7c2ea26449397c6f7c427ba3724b47cee
Camilo2324 <gamercamilo23@gmail.com>
2023-10-06 21:14:00 -0600

    Agregar archivos de proyecto.

---
+++ temp1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace temp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float f = float.Parse(textBox1.Text);
+            float c = (f - 32) * 5.0F / 9.0f;
+            textBox2.Text = c.ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            float f = float.Parse(textBox1.Text);
+            float c = (f * 9f / 5f) + 32;
+            textBox2.Text = c.ToString();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0.0";
+            textBox2.Text = "0.0";
+        }
+    }
+}
